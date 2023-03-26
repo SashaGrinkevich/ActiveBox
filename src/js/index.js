07874dbfac829459360522cb04552c0b825f63c9
SashaGrinkevich <wade00011@gmail.com>
2023-03-26 23:11:32 +0300

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
-  });
+  },
+});
