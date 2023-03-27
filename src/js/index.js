b2e993945a497d007f52bdc680e4f3be740f2d08
User <wade00011@gmail.com>
2023-03-27 22:05:27 +0300

    add

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,9 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
+    
+    // renderBullet: function (index, className) {
+    //   return '<span class="' + className + '">' + (index + 1) + "</span>";
+    // },
   },
 });
