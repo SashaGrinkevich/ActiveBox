678274d22cfb2f11afd175c44de3d1c6e1d71d72
SashaGrinkevich <wade00011@gmail.com>
2023-04-03 00:19:28 +0300

    add

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,10 @@ var swiper = new Swiper(".mySwiper", {
     // },
   },
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function(){
+    document.querySelector("header").classList.toggle("open")
+  })
+});
